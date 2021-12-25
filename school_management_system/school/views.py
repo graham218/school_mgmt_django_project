03a5b24b6c6b37dbb0d94beec4f5bf3aedc59d22
BILL GRAHAM <grahambill011@gmail.com>
2021-12-25 15:55:33 +0300

    general changes

--- school_management_system/school/views.py
+++ school_management_system/school/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def home(request):
+    title="Home"
+    context={
+        "title":title
+    }
+    return render(request, "index.html", context)
