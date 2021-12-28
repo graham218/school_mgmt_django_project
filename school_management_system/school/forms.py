7fa6cf33b1e5afe225fc6ce1e558ff0f877526f7
BILL GRAHAM <grahambill011@gmail.com>
2021-12-28 14:22:03 +0300

    forms update

--- school_management_system/school/forms.py
+++ school_management_system/school/forms.py
@@ -7,15 +7,25 @@ from django.utils.translation import gettext, gettext_lazy as _
 
 
 
+#class RegistrationForm(UserCreationForm):
+#    class Meta:
+#        model = User
+#        fields = ['username', 'email', 'password1', 'password2']
+#        widgets = {'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Username'}),
+#        'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder':'Enter Email Address'}),
+#        'password1': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder':'password1'}),
+#        'password2': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder':'password2'})}
+
 class RegistrationForm(UserCreationForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password'}))
+    password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Confirm Password'}))
+    email = forms.CharField(required=True, widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-        widgets = {'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Username'}),
-        'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder':'Enter Email Address'}),
-        'password1': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder':'password1'}),
-        'password2': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder':'password2'})}
-
+        labels = {'email': 'Email'}
+        widgets = {'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Username'})}
 
 class LoginForm(AuthenticationForm):
     username = UsernameField(widget=forms.TextInput(attrs={'autofocus': True, 'class': 'form-control'}))
--- school_management_system/school_management_system/settings.py
+++ school_management_system/school_management_system/settings.py
@@ -124,4 +124,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL='/'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
