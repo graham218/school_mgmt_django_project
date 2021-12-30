697e7ab9874d6e9388c39a28f2c9e30163bbb3e9
BILL GRAHAM <grahambill011@gmail.com>
2021-12-30 12:50:21 +0300

    Update admin.py

--- school_management_system/school/admin.py
+++ school_management_system/school/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('user', 'locality', 'city', 'state')
+    list_filter = ('city', 'state')
+    list_per_page = 10
+    search_fields = ('locality', 'city', 'state')
+
+admin.site.register(Address, AddressAdmin)
