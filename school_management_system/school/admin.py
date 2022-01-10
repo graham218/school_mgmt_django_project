18f3e94a6ef703038e13e2f429600adf5b3dac19
BILL GRAHAM <grahambill011@gmail.com>
2022-01-11 01:04:10 +0300

    admin

--- school_management_system/school/admin.py
+++ school_management_system/school/admin.py
@@ -8,4 +8,21 @@ class AddressAdmin(admin.ModelAdmin):
     list_per_page = 10
     search_fields = ('locality', 'city', 'state')
 
+class FacultyAdmin(admin.ModelAdmin):
+    list_display=('school','date_created','date_updated')
+    list_filter=('school')
+    search_fields=('school')
+
+class ProgrammesAdmin(admin.ModelAdmin):
+    list_display=('name','faculty')
+    list_filter=('name','faculty')
+    search_fields=('name')
+
+class StagesAdmin(admin.ModelAdmin):
+    list_display=('year')
+    list_filter=('year')
+    search_fields=('year')
+
 admin.site.register(Address, AddressAdmin)
+admin.site.register(Faculty, FacultyAdmin)
+admin.site.register(Programmes, ProgrammesAdmin)
