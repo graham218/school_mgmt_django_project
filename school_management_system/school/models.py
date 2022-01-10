af896748116798a7af765bd09c2f6c76d6e8bd8f
BILL GRAHAM <grahambill011@gmail.com>
2022-01-11 00:56:00 +0300

    migrations

--- school_management_system/school/models.py
+++ school_management_system/school/models.py
@@ -15,14 +15,14 @@ class Address(models.Model):
 
 class Faculty(models.Model):
     school=models.CharField(max_length=255, blank=True, null=True)
-    date_created=models.DateTimeField(auto_now_add=True, default=1)
-    date_updated=models.DateTimeField(auto_now=True, default=1)
+    date_created=models.DateTimeField(null=True, blank=True, auto_now_add=True)
+    date_updated=models.DateTimeField(null=True, blank=True, auto_now=True)
     def __str__(self):
         return self.school
 
 class Programmes(models.Model):
     name=models.CharField(max_length=255, blank=True, null=True)
-    faculty=models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    faculty=models.ForeignKey(Faculty, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.name
@@ -61,10 +61,26 @@ class Students(models.Model):
 
 class Units(models.Model):
     unit_name=models.CharField(max_length=255, blank=True, null=True)
-    date_created=models.DateTimeField(auto_now_add=True, default=1)
-    date_updated=models.DateTimeField(auto_now=True, default=1)
+    date_created=models.DateTimeField(null=True, blank=True, auto_now_add=True)
+    date_updated=models.DateTimeField(null=True, blank=True, auto_now=True)
 
     def __str__(self):
         return self.unit_name
 
 
+class Lectures(models.Model):
+    user=models.ForeignKey(User, max_length=255, on_delete=models.CASCADE)
+    lec_no=models.CharField(max_length=255, blank=True)
+    full_name=models.CharField(max_length=255, blank=False)
+    nationality=models.CharField(max_length=255, blank=True)
+    gender=models.ForeignKey(Gender, on_delete=models.CASCADE)
+    national_ID_number=models.CharField(max_length=255, blank=True)
+    phone_number=models.CharField(max_length=255, blank=True)
+    DOB=models.DateField(auto_now=False, auto_now_add=False)
+    profile_photo=models.ImageField(upload_to="lecturer_pic", blank=True )
+    postal_address=models.CharField(max_length=255, blank=True)
+    school_email=models.EmailField(max_length=255, blank=True)
+    school_email_password=models.CharField(max_length=255, blank=True)
+    total_salary_billed=models.DecimalField(max_digits=10, decimal_places=2)
+    total_salary_paid=models.DecimalField(max_digits=10, decimal_places=2)
+    balance=models.DecimalField(max_digits=10, decimal_places=2)
