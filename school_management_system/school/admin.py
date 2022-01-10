c215f3fb3b7e756f0d9dd8e5355d8d2253db5d46
BILL GRAHAM <grahambill011@gmail.com>
2022-01-11 01:21:48 +0300

    Update admin.py

--- school_management_system/school/admin.py
+++ school_management_system/school/admin.py
@@ -43,6 +43,15 @@ class UnitsAdmin(admin.ModelAdmin):
     list_filter=('unit_name')
     search_fields=('unit_name')
 
+class LectureAdmin(admin.ModelAdmin):
+    list_display = ('user', 'lec_no', 'full_name', 'nationality',
+     'gender', 'national_ID_number', 'phone_number',
+     'DOB', 'stage', 'postal_address', 'school_email', 'school_email_password',
+     'total_salary_billed', 'total_salary_paid', 'balance')
+    list_filter = ('user', 'full_name', 'lec_no')
+    list_per_page = 10
+    search_fields = ('user', 'full_name', 'lec_no')
+
 admin.site.register(Address, AddressAdmin)
 admin.site.register(Faculty, FacultyAdmin)
 admin.site.register(Programmes, ProgrammesAdmin)
@@ -50,3 +59,4 @@ admin.site.register(Stages, StagesAdmin)
 admin.site.register(Gender, GenderAdmin)
 admin.site.register(Students, StudentsAdmin)
 admin.site.register(Units, UnitsAdmin)
+admin.site.register(Lectures, LectureAdmin)
