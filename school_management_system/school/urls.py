94f1ce6d72faaa2ae767a29ddce8346dae2d46f3
BILL GRAHAM <grahambill011@gmail.com>
2022-01-11 07:10:38 +0300

    Update urls.py

--- school_management_system/school/urls.py
+++ school_management_system/school/urls.py
@@ -10,5 +10,10 @@ urlpatterns = [
     path('edit-faculty/<str:pk>', views.EditFaculty, name='edit-faculty'),
     path('add-gender/', views.AddGender, name='add-gender'),
     path('edit-gender/<str:pk>', views.EditGender, name='edit-gender'),
-    path('add-students/', views.AddStudents, name='add-students'),
+    path('add-programme/', views.AddProgramme, name='add-programme'),
+    path('edit-programme/<str:pk>', views.EditProgramme, name='edit-programme'),
+    path('add-stage/', views.AddStage, name='add-stage'),
+    path('edit-stage/<str:pk>', views.EditStage, name='edit-stage'),
+    path('add-unit/', views.AddUnit, name='add-unit'),
+    path('edit-unit/<str:pk>', views.EditUnit, name='edit-unit'),
 ]
