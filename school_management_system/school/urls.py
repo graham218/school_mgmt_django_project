05fbb3cbc0cff834078121a4c364e8727aa41afd
BILL GRAHAM <grahambill011@gmail.com>
2022-01-13 04:03:48 +0300

    updates

--- school_management_system/school/urls.py
+++ school_management_system/school/urls.py
@@ -19,7 +19,6 @@ urlpatterns = [
     path('delete-lecturer/<str:pk>', views.DeleteLecturer, name='delete-lecturer'),
     path('list-lecturer/', views.Listlectures, name='list-lecturer'),
     
-
     path('add-faculty/', views.AddFaculty, name='add-faculty'),
     path('edit-faculty/<str:pk>', views.EditFaculty, name='edit-faculty'),
     path('delete-faculty/<str:pk>', views.DeleteFaculty, name='delete-faculty'),
@@ -33,10 +32,12 @@ urlpatterns = [
     path('add-programme/', views.AddProgramme, name='add-programme'),
     path('edit-programme/<str:pk>', views.EditProgramme, name='edit-programme'),
     path('delete-programme/<str:pk>', views.DeleteProgramme, name='delete-programme'),
+    path('list-programme/', views.ListProgrammes, name='list-programme'),
 
     path('add-stage/', views.AddStage, name='add-stage'),
     path('edit-stage/<str:pk>', views.EditStage, name='edit-stage'),
     path('delete-stage/<str:pk>', views.DeleteStage, name='delete-stage'),
+    path('list-stage/', views.ListStages, name='list-stage'),
 
     path('add-unit/', views.AddUnit, name='add-unit'),
     path('edit-unit/<str:pk>', views.EditUnit, name='edit-unit'),
--- school_management_system/school/views.py
+++ school_management_system/school/views.py
@@ -549,6 +549,16 @@ def ListGender(request):
 	return render(request, "school/list-gender.html", context)
 
 @login_required
+def ListStages(request):
+	title = 'List of All Stages'
+	queryset = Stages().objects.all()
+	context = {
+	"title": title,
+	"queryset": queryset,
+	}
+	return render(request, "school/list-stages.html", context)
+
+@login_required
 def ShowAddress(request):
 	title = 'My Address'
 	queryset = Address().objects.filter(user=request.user)
