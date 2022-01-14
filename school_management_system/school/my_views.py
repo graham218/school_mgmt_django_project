a50ae449da18ad99e53743a3c996b79ebce63b2d
BILL GRAHAM <grahambill011@gmail.com>
2022-01-14 06:19:53 +0300

    updates

--- school_management_system/school/my_views.py
+++ school_management_system/school/my_views.py
@@ -6,3 +6,14 @@ import csv
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
+def UserProfile(request):
+    title="Student's Profile"
+    title2="Lecturer's Profile"
+    my_queryset=Students.objects.get(user=request.user)
+    lec_queryset=Lectures.objects.get(user=request.user)
+    context={
+        "title": title,
+        "my_queryset": my_queryset,
+        "lec_queryset": lec_queryset
+    }
+    return render(request, "navbar.html", context)
