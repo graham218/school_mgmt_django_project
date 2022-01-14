8f1f98844f6e219e15192e6c87209fe715d39268
BILL GRAHAM <grahambill011@gmail.com>
2022-01-15 02:30:42 +0300

    updates

--- school_management_system/school/my_views.py
+++ school_management_system/school/my_views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import *
 from .models import *
 from django.http import HttpResponse
 import csv
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+
+from django.conf import settings
+from django.urls import reverse
+from paypal.standard.forms import PayPalPaymentsForm
 
 @login_required
 def EditStudentsProfile(request, pk):
@@ -24,3 +28,18 @@ def EditStudentsProfile(request, pk):
         "form": form
     }
     return render(request, 'school/create-edit-students.html', context)
+
+def payment_process(request):
+    host = request.get_host()
+    paypal_dict = {
+        'business': settings.PAYPAL_RECEIVER_EMAIL,
+        'amount': '100',
+        'item_name': 'Item_Name_xyz',
+        'invoice': 'Test Payment Invoice',
+        'currency_code': 'USD',
+        'notify_url': 'http://{}{}'.format(host, reverse('paypal-ipn')),
+        'return_url': 'http://{}{}'.format(host, reverse('payment_done')),
+        'cancel_return': 'http://{}{}'.format(host, reverse('payment_canceled')),
+    }
+    form = PayPalPaymentsForm(initial=paypal_dict)
+    return render(request, 'pets/payment_process.html', {'form': form})
