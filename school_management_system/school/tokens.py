a765c91a58efde0353bbbff766eb355ef85f6391
BILL GRAHAM <grahambill011@gmail.com>
2022-01-17 00:31:35 +0300

    Create tokens.py

---
+++ school_management_system/school/tokens.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils import six
+
+
+class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
+    def _make_hash_value(self, user, timestamp):
+        return (
+            six.text_type(user.pk) + six.text_type(timestamp) +
+            six.text_type(user.profile.email_confirmed)
+        )
+
+
+class PasswordResetToken(PasswordResetTokenGenerator):
+    def _make_hash_value(self, user, timestamp):
+        return (
+            six.text_type(user.pk) + six.text_type(timestamp) +
+            six.text_type(user.profile.reset_password)
+        )
+
+
+account_activation_token = AccountActivationTokenGenerator()
+password_reset_token = PasswordResetToken()
