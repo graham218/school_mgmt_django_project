9c9aa1bf99247648331db539f3b2fbd7e1755eea
BILL GRAHAM <grahambill011@gmail.com>
2022-01-19 06:18:53 +0300

    updates

--- school_management_system/school/my_tests1/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
-
-urlpatterns=[
-    path('reset-password', PasswordResetView.as_view(), name='password_reset'),
-    path('reset-password/done', PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset-password/confirm/<uidb64>[0-9A-Za-z]+)-<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'), 
-    path('reset-password/complete/',PasswordResetCompleteView.as_view(),name='password_reset_complete'),
-]
