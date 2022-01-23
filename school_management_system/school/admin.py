5ef01aa083a574a3b9cb721f466300d621a6b9c3
BILL GRAHAM <grahambill011@gmail.com>
2022-01-23 09:00:34 +0300

    Update admin.py

--- school_management_system/school/admin.py
+++ school_management_system/school/admin.py
@@ -2,60 +2,72 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+
+
 class AddressAdmin(admin.ModelAdmin):
     list_display = ('user', 'locality', 'city', 'state')
     list_filter = ('city', 'state')
     list_per_page = 10
     search_fields = ('locality', 'city', 'state')
 
+
 class FacultyAdmin(admin.ModelAdmin):
-    list_display=['school','date_created','date_updated']
-    list_filter=['school']
-    search_fields=['school']
+    list_display = ['school', 'date_created', 'date_updated']
+    list_filter = ['school']
+    search_fields = ['school']
+
 
 class ProgrammesAdmin(admin.ModelAdmin):
-    list_display=['name','faculty']
-    list_filter=['name','faculty']
-    search_fields=['name']
+    list_display = ['name', 'faculty']
+    list_filter = ['name', 'faculty']
+    search_fields = ['name']
+
 
 class StagesAdmin(admin.ModelAdmin):
-    list_display=['year']
-    list_filter=['year']
-    search_fields=['year']
+    list_display = ['year']
+    list_filter = ['year']
+    search_fields = ['year']
+
 
 class GenderAdmin(admin.ModelAdmin):
-    list_display=['gender']
-    list_filter=['gender']
-    search_fields=['gender']
+    list_display = ['gender']
+    list_filter = ['gender']
+    search_fields = ['gender']
+
 
 class StudentsAdmin(admin.ModelAdmin):
     list_display = ['user', 'admission_no', 'full_name', 'nationality',
-     'stud_gender', 'national_ID_number', 'birth_cert_no', 'phone_number',
-     'DOB', 'date_of_admission', 'date_of_graduation', 'programme',
-     'stage', 'postal_address', 'school_email', 'school_email_password',
-     'total_fees_billed', 'total_fees_paid', 'balance']
+                    'stud_gender', 'national_ID_number', 'birth_cert_no', 'phone_number',
+                    'DOB', 'date_of_admission', 'date_of_graduation', 'programme',
+                    'stage', 'postal_address', 'school_email', 'school_email_password',
+                    'total_fees_billed', 'total_fees_paid', 'balance']
     list_filter = ['user', 'full_name', 'admission_no']
     list_per_page = 10
     search_fields = ['admission_no', 'user', 'full_name']
 
+
 class UnitsAdmin(admin.ModelAdmin):
-    list_display=['unit_name','date_created','date_updated']
-    list_filter=['unit_name']
-    search_fields=['unit_name']
+    list_display = ['unit_name', 'date_created', 'date_updated']
+    list_filter = ['unit_name']
+    search_fields = ['unit_name']
+
 
 class LectureAdmin(admin.ModelAdmin):
     list_display = ['user', 'lec_no', 'full_name', 'nationality',
-     'lec_gender', 'national_ID_number', 'phone_number',
-     'DOB','postal_address', 'school_email', 'school_email_password',
-     'total_salary_billed', 'total_salary_paid', 'balance']
+                    'lec_gender', 'national_ID_number', 'phone_number',
+                    'DOB', 'postal_address', 'school_email', 'school_email_password',
+                    'total_salary_billed', 'total_salary_paid', 'balance']
     list_filter = ['user', 'full_name', 'lec_no']
     list_per_page = 10
     search_fields = ['user', 'full_name', 'lec_no']
 
+
 class Marks(admin.ModelAdmin):
-    list_display = []
+    list_display = ['user', 'first_name', 'last_name',
+                    'stage', 'unit_or_subject_name', 'marks', 'grade']
     list_per_page = 10
     search_fields = []
+
 
 admin.site.register(Address, AddressAdmin)
 admin.site.register(Faculty, FacultyAdmin)
