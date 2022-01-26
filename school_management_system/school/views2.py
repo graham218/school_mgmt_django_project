b925aa1d30468687db55d0d83065e12c3979843d
BILL GRAHAM <grahambill011@gmail.com>
2022-01-27 02:44:47 +0300

    Create views2.py

---
+++ school_management_system/school/views2.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import *
+from .forms2 import *
+from .models import *
+from django.http import HttpResponse, HttpResponseRedirect
+import csv
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+from django.conf import settings
+from django.urls import reverse
