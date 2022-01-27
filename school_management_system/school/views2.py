5905265837e1497c2e1df9a052c5e8950710e488
BILL GRAHAM <grahambill011@gmail.com>
2022-01-27 04:54:58 +0300

    Update views2.py

--- school_management_system/school/views2.py
+++ school_management_system/school/views2.py
@@ -12,7 +12,7 @@ from django.urls import reverse
 
 #=====================================================================
 def FeeReceiptList(request):
-    title = 'List All Fee Receipts'
+    title = 'List All Fee Student Receipts'
     form = FeeReceiptSearchForm(request.POST or None)
     queryset = FeeReceipt.objects.all()
     context = {
@@ -20,20 +20,7 @@ def FeeReceiptList(request):
         "queryset": queryset,
     }
     if request.method == 'POST':
-        queryset = FeeReceipt.objects.filter(stage=form['stage'].value(), user=form['user'].value(),
-                                            unit_or_subject_name=form['unit_or_subject_name'].value(), full_name__icontains=form['full_name'])
-        if form['export_to_CSV'].value() == True:
-            response = HttpResponse(content_type='text/csv')
-            response['Content-Disposition'] = 'attachment; filename="List of Registered Students.csv"'
-            writer = csv.writer(response)
-            writer.writerow(['USERNAME', 'STUDENT NAME', 'STAGE', 'UNIT NAME',
-                            'MARKS', 'GRADE', 'DATE REAGISTERED', 'DATE UPDATED'])
-            instance = queryset
-            for student in instance:
-                writer.writerow([student.user, student.full_name, student.stage,
-                                 student.unit_or_subject_name, student.marks, student.grade,
-                                 student.date_created, student.date_updated])
-            return response
+        queryset = FeeReceipt.objects.filter(user=form['user'].value())
 
     context = {
         "form": form,
