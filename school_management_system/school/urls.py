831191c5df7af8617d3ee642ed39cd23fd3f38a2
BILL GRAHAM <grahambill011@gmail.com>
2022-02-02 23:17:43 +0300

    Update urls.py

--- school_management_system/school/urls.py
+++ school_management_system/school/urls.py
@@ -88,5 +88,6 @@ urlpatterns = [
     path('SpecialExamList/', views2.SpecialExamList, name='SpecialExamList'),
     path('lecturer_units/', views2.lecturer_units, name='lecturer_units'),
     path('lecturer_units_edit/<str:pk>', views2.lecturer_units_edit, name='lecturer_units_edit'),
-    path('lecturer_units_delete/', views2.lecturer_units_delete, name='lecturer_units_delete'),
+    path('lecturer_units_delete/<str:pk>', views2.lecturer_units_delete, name='lecturer_units_delete'),
+    path('ListAllLecturerUnits/', views2.ListAllLecturerUnits, name='ListAllLecturerUnits'),
 ]
