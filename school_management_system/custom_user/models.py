3bcf4c516cbe717730c32bcc21c1784a6b8ffaf2
BILL GRAHAM <grahambill011@gmail.com>
2022-02-08 18:10:56 +0300

    u

--- school_management_system/custom_user/models.py
+++ school_management_system/custom_user/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
+    profile_photo = models.ImageField(upload_to="profile_pics/", blank=True)
     is_admin = models.BooleanField(default=False)
     is_student = models.BooleanField(default=False)
     is_lecturer = models.BooleanField(default=False)
