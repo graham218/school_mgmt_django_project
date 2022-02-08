1f77c8ecc18a23889b8f97d73632de833bc9b8a9
BILL GRAHAM <grahambill011@gmail.com>
2022-02-08 16:19:10 +0300

    updates

--- school_management_system/custom_user/models.py
+++ school_management_system/custom_user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    is_student = models.BooleanField(default=False)
+    is_lecturer = models.BooleanField(default=False)
+    is_supplier = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_non_staff = models.BooleanField(default=False)
