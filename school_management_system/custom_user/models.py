ce82fd2ac062f19e5193741ef4aaf19b12162a60
BILL GRAHAM <grahambill011@gmail.com>
2022-02-08 18:31:08 +0300

    updates

--- school_management_system/custom_user/models.py
+++ school_management_system/custom_user/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
+    first_name=models.CharField(max_length=255, blank=False, null=False, default="John")
+    middle_name=models.CharField(max_length=255, blank=False, null=False, default="Doe")
+    last_name=models.CharField(max_length=255, blank=False, null=False, default="Smith")
     profile_photo = models.ImageField(upload_to="profile_pics/", blank=True)
     is_admin = models.BooleanField(default=False)
     is_student = models.BooleanField(default=False)
