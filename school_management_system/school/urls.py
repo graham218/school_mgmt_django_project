93c0e631acd8667514b0b15ab45d7100ac7ba637
BILL GRAHAM <grahambill011@gmail.com>
2022-02-11 05:10:11 +0300

    updates

--- school_management_system/school/urls.py
+++ school_management_system/school/urls.py
@@ -89,7 +89,7 @@ urlpatterns = [
     path('lecturer_units_delete/<str:pk>', views2.lecturer_units_delete, name='lecturer_units_delete'),
     path('ListAllLecturerUnits/', views2.ListAllLecturerUnits, name='ListAllLecturerUnits'),
 
-    path('seats/', views2.seats, name='seats'),
+    path('add_seats/', views2.seats, name='add_seats'),
     path('list_seats/', views2.list_seats, name='list_seats'),
     path('seats_edit/<str:pk>', views2.seats_edit, name='seats_edit'),
     path('seats_delete/<str:pk>', views2.seats_delete, name='seats_delete'),
