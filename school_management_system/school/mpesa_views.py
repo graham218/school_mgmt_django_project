6cb674be202a10a34dda5cdbcab9506ab72a3bcf
graham218 <grahambill011@gmail.com>
2022-02-13 23:24:08 +0300

    updates

--- school_management_system/school/mpesa_views.py
+++ school_management_system/school/mpesa_views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
+from .forms2 import FeePaymentForm
 
 def pay_fee(request):
     title="Pay School Fee Using Mpesa"
     button="Pay School Fee"
+    form=FeePaymentForm(request.POST or None)
     context={
         "title":title,
+        "form": form,
         "button":button
     }
     return render(request, "mpesa_payment/fee_payment.html",context)
