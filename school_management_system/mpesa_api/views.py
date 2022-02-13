43a329b2491de877e6ba28423a255121ed1fca79
graham218 <grahambill011@gmail.com>
2022-02-13 18:25:17 +0300

    updates

--- school_management_system/mpesa_api/views.py
+++ school_management_system/mpesa_api/views.py
@@ -47,8 +47,6 @@ def register_urls(request):
     headers = {"Authorization": "Bearer %s" % access_token}
     options = {"ShortCode": LipanaMpesaPassword.Business_short_code,
                "ResponseType": "Completed",
-            #    "ConfirmationURL": "http://127.0.0.1:8000/api/v1/c2b/confirmation",
-            #    "ValidationURL": "http://127.0.0.1:8000/api/v1/c2b/validation"}
                "ConfirmationURL": "https://4e28-41-89-192-24.ngrok.io/api/v1/c2b/confirmation",
                "ValidationURL": "https://4e28-41-89-192-24.ngrok.io/api/v1/c2b/validation"}
     response = requests.post(api_url, json=options, headers=headers)
