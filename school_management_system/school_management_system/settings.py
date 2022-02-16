133e7831f5899e61d9b7fcbe00a35391ce090c3c
graham218 <grahambill011@gmail.com>
2022-02-16 05:17:59 +0300

    Update settings.py

--- school_management_system/school_management_system/settings.py
+++ school_management_system/school_management_system/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -156,3 +158,7 @@ EMAIL_USE_TLS = False
 DEFAULT_FROM_EMAIL = 'testing@example.com'
 
 CRISPY_TEMPLATE_PACK = 'uni_form'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
