54489f1ee1ab08e464a39d7daca52ce1ed81ab55
graham218 <grahambill011@gmail.com>
2022-02-17 16:30:07 +0300

    Update views.py

--- school_management_system/school/views.py
+++ school_management_system/school/views.py
@@ -83,6 +83,32 @@ def home(request):
     }
     return render(request, "index.html", context)
 
+def all_users(request):
+    all_system_users=User.objects.all()
+    title="All Users Registered"
+    context={
+        "all_system_users":all_system_users,
+        "title":title,
+    }
+    return render(request, "admin/all_users.html", context)
+
+def all_students(request):
+    students=Students.objects.all()
+    title="All Students Registered"
+    context={
+        "students":students,
+        "title":title,
+    }
+    return render(request, "admin/all_students.html", context)
+
+def all_lecturers(request):
+    lecturers=Lectures.objects.all()
+    title="All Lecturers Registered"
+    context={
+        "lecturers":lecturers,
+        "title":title,
+    }
+    return render(request, "admin/all_lecturers.html", context)
 
 # Authentication Starts Here
 
