2b787f5c8de740bbd60b30e355ad503ae2e0ab70
graham218 <grahambill011@gmail.com>
2022-03-08 01:04:24 +0300

    updates

--- school_management_system/base/urls.py
+++ school_management_system/base/urls.py
@@ -4,11 +4,11 @@ from . import views
 app_name='base'
 
 urlpatterns = [
-    path('lobby/', views.lobby),
-    path('room/', views.room),
-    path('get_token/', views.getToken),
+    path('lobby/', views.lobby, name='lobby'),
+    path('room/', views.room, name='room'),
+    path('get_token/', views.getToken, name='get_token'),
 
-    path('create_member/', views.createMember),
-    path('get_member/', views.getMember),
-    path('delete_member/', views.deleteMember),
+    path('create_member/', views.createMember, name='create_member'),
+    path('get_member/', views.getMember, name='get_member'),
+    path('delete_member/', views.deleteMember, name='delete_member'),
 ]
