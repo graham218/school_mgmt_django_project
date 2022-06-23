d1d11b23ee1c0dcc6a9470cab4378b7a2a277875
graham218 <grahambill011@gmail.com>
2022-06-23 14:13:21 +0300

    Update views.py

--- school_management_system/my_mpesa/views.py
+++ school_management_system/my_mpesa/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django_daraja.mpesa.core import MpesaClient
 
-# Create your views here.
+def index(request):
+    cl = MpesaClient()
+    # Use a Safaricom phone number that you have access to, for you to be able to view the prompt.
+    phone_number = '07xxxxxxxx'
+    amount = 1
+    account_reference = 'reference'
+    transaction_desc = 'Description'
+    callback_url = request.build_absolute_uri(reverse('mpesa_stk_push_callback'))
+    response = cl.stk_push(phone_number, amount, account_reference, transaction_desc, callback_url)
+    return HttpResponse(response)
+
+def stk_push_callback(request):
+        data = request.body
+        # You can do whatever you want with the notification received from MPESA here.
